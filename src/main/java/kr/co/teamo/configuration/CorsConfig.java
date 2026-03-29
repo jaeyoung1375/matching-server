839a304771c9392099b0363b953a3a821405e4a3
jaeyoung <jaeyoung1375@naver.com>
2026-03-29 19:53:18 +0900

    cors 수정

--- src/main/java/kr/co/teamo/configuration/CorsConfig.java
+++ src/main/java/kr/co/teamo/configuration/CorsConfig.java
@@ -15,10 +15,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")               // 모든 URL에 대해
-		                .allowedOriginPatterns(
-		                	    "http://localhost:*",
-		                	    "http://146.56.116.158:*"
-		                	)
+		                .allowedOrigins(
+		                        "http://localhost:3000",
+		                        "http://168.107.63.120:3000"
+		                )
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                         .allowedHeaders("*")             // 모든 헤더 허용
                         .allowCredentials(true)       // 인증정보 포함 여부 (쿠키 등), false면 allowedOrigins에 * 가능
